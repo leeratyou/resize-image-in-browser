9a6908916f27117a62620ae4238dd64f5dc18301
boris <veligorya.boris@gmail.com>
2023-07-01 07:23:00 +0300

    Fix typings

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,9 @@
-enum Format {
-  png = 'png',
-  jpeg = 'jpeg',
-  webp = 'webp',
-  bmp = 'bmp'
-}
-
-enum Output {
-  dataURL = 'dataURL',
-  File = 'File',
-  Blob = 'Blob'
-}
-
-interface Options {
+export interface Options {
   maxWidth?: number
   maxHeight?: number
-  format?: Format,
+  format?: 'jpeg' | 'png' | 'webp' | 'bmp',
   quality?: number,
-  output?: Output
+  output?: 'dataURL' | 'File' | 'Blob'
 }
 
 export function resize(file: any, options: Options): Promise<any> {
@@ -27,20 +14,20 @@ export function resize(file: any, options: Options): Promise<any> {
     quality = 1.0,
     output = 'dataURL'
   } = options
-  
+
   const image = new Image()
   const url = typeof file === 'string' ? file : URL.createObjectURL(file)
-  
+
   return new Promise((resolve, reject) => {
     image.onload = () => {
       const canvas = document.createElement('canvas')
       const ctx = canvas.getContext('2d')
-      
+
       if (!ctx) return reject('Error')
-      
+
       let width = image.width
       let height = image.height
-      
+
       if (width > height) {
         if (width > maxWidth) {
           height *= maxWidth / width
@@ -52,21 +39,21 @@ export function resize(file: any, options: Options): Promise<any> {
           height = maxHeight
         }
       }
-      
+
       canvas.width = width
       canvas.height = height
-      
+
       ctx.drawImage(image, 0, 0, width, height)
-      
+
       if (output === "dataURL") {
         const dataURL = canvas.toDataURL(`image/${format}`, quality)
         resolve(dataURL)
       }
-      
+
       if (output === 'Blob') {
         canvas.toBlob(blob => resolve(blob), `image/${format}`, quality)
       }
-      
+
       if (output === 'File') {
         canvas.toBlob(blob => {
           if (!blob) return reject('Error')
